ed3c293440853159f65c18a2530b50d952d99a1d
D.Serikbayev <dauser200606@gmail.com>
2024-01-28 09:44:18 +0600

    session 1

---
+++ tsis 2/examples/lists/access list items.py
@@ -0,0 +1,39 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+
+#
+#
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+
+#
+#
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+#
+#
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4]) # from start to n
+
+#
+#
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:]) # from n to end
+
+#
+#
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])
+
+#
+#
+
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
